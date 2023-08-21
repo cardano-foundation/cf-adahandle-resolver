7a78a7954298f913ce3ceb460d232cc2950a1576
Satya <satran004@gmail.com>
2023-08-21 11:43:22 +0800

    chore: Enabled component scan at package root instead of specific package

--- src/main/java/org/cardanofoundation/tools/adahandle/resolver/AdaHandleResolverApplication.java
+++ src/main/java/org/cardanofoundation/tools/adahandle/resolver/AdaHandleResolverApplication.java
@@ -2,10 +2,12 @@ package org.cardanofoundation.tools.adahandle.resolver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@ComponentScan({"org.cardanofoundation.tools.adahandle.resolver.repository", "org.cardanofoundation.tools.adahandle.resolver.storage"})
+@EnableJpaRepositories
+@EntityScan
 public class AdaHandleResolverApplication {
 
     public static void main(String[] args) {
